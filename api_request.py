c5e0476042e2c6692287e44293c958fe38b8aa9a
pmoriart <moriarty.pam@gmail.com>
2018-06-07 18:14:58 -0700

    create request fx

---
+++ api_request.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  7 17:29:58 2018
+
+@author: pamela
+@purpose: slow rate of api requests
+"""
+import time
+
+def api_request(msg, slp=1):
+    '''A wrapper to make robust https requests.'''
+    status_code = 500  # Want to get a status-code of 200
+    while status_code != 200:
+        time.sleep(slp)  # Don't ping the server too often
+        try:
+            r = requests.get(msg)
+            status_code = r.status_code
+            if status_code != 200:
+                print("Server Error! Response Code %i. Retrying..." % (r.status_code))
+        except:
+            print("An exception has occurred, probably a momentory loss of connection. Waiting one seconds...")
+            time.sleep(1)
+    return r
+
--- pull_images.py
+++ pull_images.py
@@ -8,8 +8,8 @@ Created on Thu Jun  7 14:31:32 2018
 import urllib
 
 image_dir = "/media/pamela/Stuff/game_images/"
-ngames = len(game_info)
-for n in range(ngames):
+n_games = len(game_info)
+for n in range(n_games):
     name_tmp = ids_exist[n]
     file_name_tmp = image_dir + str(name_tmp) + ".jpg"
     urllib.request.urlretrieve(game_info[n][2], file_name_tmp)
